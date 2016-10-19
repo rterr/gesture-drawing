bd93c35c8a4650ca99bf402f929c1630cc059296
mrmojica <mrmojica510@gmail.com>
2016-10-19 11:45:08 -0700

    category + timer selection working

--- client/components/menu.js
+++ client/components/menu.js
@@ -3,14 +3,38 @@ var ReactDOM = require('react-dom');
 var connect = require('react-redux').connect;
 var actions = require('../actions');
 
+ // var category = "hands";
+ //        var timer = 30;
+
 var Menu = React.createClass({
+    getInitialState: function() {
+      return {
+        category: "hands",
+        timer: 30
+      };
+    },
+
+    categoryChanged: function(e) {
+      this.setState({
+        category: e.currentTarget.value
+      });
+      console.log('category state', this.state.category);
+    },
+
+    timerChanged: function(e) {
+      this.setState({
+        timer: e.currentTarget.value
+      });
+      console.log('timer state', this.state.timer);
+    },
+
     startSession: function(event) {
         event.preventDefault();
         //Temporary parameter declaration until I figure out those radio buttons
-        var category = "hands";
-        var timer = 30;
+        // var category = "hands";
+        // var timer = 30;
         console.log("start session");
-        this.props.dispatch(actions.startSession(category, timer));
+        this.props.dispatch(actions.startSession(this.state.category, this.state.timer));
         this.props.dispatch(actions.fetchImages());
     },
 
@@ -46,19 +70,19 @@ var Menu = React.createClass({
             <span>What would you like to draw?</span>
             <div className="category">
               <label>
-              <input type="radio" value="hands" name="category" ref="category" />
+              <input type="radio" value="hands" name="category" ref="category" onChange={this.categoryChanged} />
               Hands
               </label>
               </div>
               <div className="radio">
               <label>
-              <input type="radio" value="heads" name="category" ref="category" />
+              <input type="radio" value="heads" name="category" ref="category" onChange={this.categoryChanged} />
               Heads
               </label>
               </div>
               <div className="radio">
               <label>
-              <input type="radio" value="torsos" name="category" ref="category" />
+              <input type="radio" value="torsos" name="category" ref="category" onChange={this.categoryChanged} />
               Torsos
               </label>
               </div>
@@ -66,19 +90,19 @@ var Menu = React.createClass({
               <span>For how long should each image stay?</span>
               <div className="timer">
                 <label>
-                <input type="radio" name="timer" value="30" />
+                <input type="radio" name="timer" value="30" onChange={this.timerChanged} />
                 30 Seconds
                 </label>
                 </div>
                 <div className="radio">
                 <label>
-                <input type="radio" name="timer" value="60" />
+                <input type="radio" name="timer" value="60" onChange={this.timerChanged} />
                 1 Minute
                 </label>
                 </div>
                 <div className="radio">
                 <label>
-                <input type="radio" name="timer" value="120" />
+                <input type="radio" name="timer" value="120" onChange={this.timerChanged} />
                 2 Minutes
                 </label>
                 </div>
